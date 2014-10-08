17be1e57b15dd0e5582f75b3ff4e5b7ed58d9611
Elaine Sun <lansun@adobe.com>
2014-10-07 19:11:03 -0700

    Initial commit of a set of small bash shell scripts that help troubleshoot AEM production issues on *nix platform.

--- cq/troubleshoot/get_most_frequent_requests.sh
+++ cq/troubleshoot/get_most_frequent_requests.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-# author  Elaine Sun, Adobe
-# since   2013-09-20
+
+#############################################################################
+# Copyright 2013 Adobe
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#  limitations under the License.
+# 
+#############################################################################
+
+#  original author  Elaine Sun, Adobe
+#  since 2014-09-05
 
 request_log=$1
 num=$2
--- cq/troubleshoot/get_slowest_requests.sh
+++ cq/troubleshoot/get_slowest_requests.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-# author  Elaine Sun, Adobe
+
+#############################################################################
+# Copyright 2013 Adobe
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#############################################################################
+
+# original author  Elaine Sun, Adobe
 # since   2013-09-20
 
 request_log=$1
