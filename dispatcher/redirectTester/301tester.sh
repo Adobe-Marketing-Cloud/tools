34405df3ff328709ae4753ed5f74b54b5a9ecc56
Ian Reasor <ireasor@adobe.com>
2015-02-23 16:21:21 -0600

    Added copyright notice to redirect tester.

--- dispatcher/redirectTester/301tester.sh
+++ dispatcher/redirectTester/301tester.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+
+#############################################################################
+# Copyright 2015 Adobe
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  original author  Ian Reasor, Adobe
+#  since   2015-02-23
+#############################################################################
+
 #Checks redirects.  Expects an input file in the format of oldUrl|newUrl.
 #Note that the input file MUST end with a new line.
 #Will check for correct redirect status and destination, outputting the results in a greppable format.
 #Run as ./301tester.sh myInputFile.txt >> outputLog.txt
+
 IFS='|';
 REDIRECT_STATUS="301";
 
